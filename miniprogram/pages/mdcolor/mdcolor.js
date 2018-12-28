75f2befc37ccfab704f0085d6d48e71bf33394e0
Bestony <best.tony@foxmail.com>
2018-12-28 21:27:51 +0800

    feat: add MD Color

---
+++ miniprogram/pages/mdcolor/mdcolor.js
@@ -0,0 +1,38 @@
+// pages/mdcolor/mdcolor.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.showToast({
+      title: '下拉查看所有 Color Palettes!',
+      icon:'none'
+    })
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: "用配色助手在手机上就能查 Material Design Color Palettes!",
+      path: "pages/mdcolor/mdcolor",
+      imageUrl: "https://postimg.aliavv.com/201810/xk77q.png"
+    }
+  }
+})
