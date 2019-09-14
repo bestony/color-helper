a4ea6c3779b4c04db151b0fb11ded6dbec621cc4
Bestony <best.tony@foxmail.com>
2019-09-14 23:06:05 +0800

    feat: add some comment

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,8 +1,14 @@
 var mta = require('miniprogram_npm/mta-wechat-analysis/index.js');
 App({
   onLaunch: function (options) {
+    /**
+     * 获取系统信息，用于后续处理 Block 高度
+     */
     let systeminfo = wx.getSystemInfoSync();
 
+    /**
+     * 初始化云开发
+     */
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -10,6 +16,9 @@ App({
         traceUser: true,
       })
     }
+    /**
+     * 初始化 MTA
+     */
     mta.App.init({
       "appID": "500664522",
       "eventID": "500664523",
